dcd9399aab123fe704caf2d53ad70bb81c0c7f3e
mansouraldundur@gmail.com <>
2021-07-28 00:48:01 +0300

    fix: migrations order

--- resources/views/Posts/create.blade.php
+++ resources/views/Posts/create.blade.php
@@ -13,15 +13,15 @@
     <div class="edit__form__container public__news__card__container">
         <form action="{{ route("posts.store" ) }}" method="post" id="news__creation__form">
             @csrf
-            <h3 style="color:#9e39e8">ADD NEW TITLE</h3>
+            <h3 style="color:#045FD0">ADD NEW TITLE</h3>
             <input type="text" name="title" class="my__input__text__field"/>
-            <h3 style="color:#9e39e8">CHOOSE A CATEGORY</h3>
+            <h3 style="color:#045FD0">CHOOSE A CATEGORY</h3>
             <select name="category_id" class="my__input__text__field">
                 @foreach($categories as $category)
                     <option value="{{$category->id}}"> {{$category->name}} </option>
                 @endforeach
             </select>
-            <h3 style="margin-bottom:  1em ; color:#9e39e8">EDIT THE CONTENT </h3>
+            <h3 style="margin-bottom:  1em ; color:#045FD0">EDIT THE CONTENT </h3>
             <textarea class="editor" style="height:50px" name="content"></textarea>
             <input type="number" name="user_id" value="{{Auth::user()->id}}" hidden>
 
